5042fd0ebc00296969f787add5266e4f756f377c
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-07-23 18:19:53 +0700

    Add LAB03 Assignment

---
+++ LAB03/Assignment/Q-01.sql
@@ -0,0 +1,9 @@
+/* เขียน SQL statement เพื่อเพิ่มข้อมูลลงในตาราง LAB_EMP
+   โดยใช้ข้อมูลแถวแรกจากตารางที่กำหนดให้ โดยไม่ต้องระบุชื่อคอลัมน์ */
+
+INSERT INTO lab_emp
+VALUES (1, "Pastel", "Lorenze", "lpastel", 895),
+        (2, "Dansk", "Patty", "pdansk", 860),
+        (3, "Brooks", "Ben", "bbrooks", 1100),
+        (4, "Newton", "Ched", "cnewton", 750),
+        (5, "Raff", "Audrey", "araff", 1550);
---
+++ LAB03/Assignment/Q-09.sql
@@ -0,0 +1,3 @@
+/* เขียน SQL statement เพื่อลบข้อมูลทุกแถวในตาราง lab_emp */
+
+DELETE FROM lab_emp;
