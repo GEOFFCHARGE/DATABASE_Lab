0a08a45bc7388a5e15c9a84de189bfd6b32fbb1e
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-08-05 09:20:04 +0700

    Update Q-01.sql

--- LAB03/Assignment/Q-01.sql
+++ LAB03/Assignment/Q-01.sql
@@ -2,8 +2,4 @@
    โดยใช้ข้อมูลแถวแรกจากตารางที่กำหนดให้ โดยไม่ต้องระบุชื่อคอลัมน์ */
 
 INSERT INTO lab_emp
-VALUES (1, "Pastel", "Lorenze", "lpastel", 895),
-        (2, "Dansk", "Patty", "pdansk", 860),
-        (3, "Brooks", "Ben", "bbrooks", 1100),
-        (4, "Newton", "Ched", "cnewton", 750),
-        (5, "Raff", "Audrey", "araff", 1550);
+VALUES (1, "Pastel", "Lorenze", "lpastel", 895);
