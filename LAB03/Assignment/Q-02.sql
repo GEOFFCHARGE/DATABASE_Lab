b9a4fe6b847cd2305bae0c8ea2e1ea4a8f9680d5
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-08-05 09:20:27 +0700

    Update Q-02.sql

--- LAB03/Assignment/Q-02.sql
+++ LAB03/Assignment/Q-02.sql
@@ -2,7 +2,4 @@
    โดยใช้ข้อมูลแถวที่สองจากตารางที่กำหนดให้ โดยต้องระบุชื่อคอลัมน์ */
 
 INSERT INTO lab_emp(id, last_name, first_name, userid, salary)
-VALUES (2, "Dansk", "Patty", "pdansk", 860),
-        (3, "Brooks", "Ben", "bbrooks", 1100),
-        (4, "Newton", "Ched", "cnewton", 750),
-        (5, "Raff", "Audrey", "araff", 1550);
+VALUES (2, "Dansk", "Patty", "pdansk", 860);
